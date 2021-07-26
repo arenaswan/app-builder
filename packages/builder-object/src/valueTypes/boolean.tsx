a8ef8b7df45c9f5a4370b648a8762c4a90b9767e
廖大雪 <2291335922@qq.com>
2021-07-26 12:11:20 +0800

    #199 boolean及toggle字段类型配置默认值未能生效

--- packages/builder-object/src/valueTypes/boolean.tsx
+++ packages/builder-object/src/valueTypes/boolean.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { CheckIcon } from '@chakra-ui/icons'
 import { Checkbox } from 'antd';
 import Button from '@salesforce/design-system-react/components/button'; 
@@ -36,13 +36,17 @@ export const boolean = {
   renderFormItem: (text: any, props: any) => {
     const { fieldProps } = props;
     const { onChange: formOnChange } = fieldProps;
-    let value = !isNil(fieldProps.value) ? fieldProps.value : props.text;
+    let value: boolean = !isNil(fieldProps.value) ? fieldProps.value : !!props.text;
     // 添加useState的原因是 列表视图 和grid类型下boolean子字段不能来回切换其值。
     const [checked, setChecked] = useState(value);
     function onChange(e) {
       formOnChange(e.target.checked);
       setChecked(e.target.checked);
     }
+    // fix 如果含有默认值， const [checked, setChecked] = useState(value); 这行代码只会实行一次的bug。
+    useEffect(()=>{
+      setChecked(value);
+    },[value])
     return (<Checkbox onChange={onChange} checked={checked}></Checkbox>)
   }
 }
--- packages/builder-object/src/valueTypes/toggle.tsx
+++ packages/builder-object/src/valueTypes/toggle.tsx
@@ -1,4 +1,4 @@
-import React, { useState }  from 'react'
+import React, { useEffect, useState }  from 'react'
 import { CheckIcon } from '@chakra-ui/icons'
 import { Switch } from "antd";
 import { isNil } from 'lodash'
@@ -22,12 +22,16 @@ export const toggle = {
   renderFormItem: (_: any, props: any) => {
     const { fieldProps } = props;
     const { onChange: formOnChange } = fieldProps;
-    let value = !isNil(fieldProps.value) ? fieldProps.value : props.text;
+    let value: boolean = !isNil(fieldProps.value) ? fieldProps.value : !!props.text;
     const [checked, setChecked] = useState(value);
     function onChange(check: boolean,event: Event) {
       formOnChange(check);
       setChecked(check);
     }
+    // fix 如果含有默认值， const [checked, setChecked] = useState(value); 这行代码只会实行一次的bug。
+    useEffect(() => {
+      setChecked(value);
+    }, [value])
     return (<Switch onChange={onChange} checked={checked}></Switch>)
   }
 }
