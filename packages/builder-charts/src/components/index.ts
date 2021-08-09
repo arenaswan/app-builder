1b9c990c54b66f708ab71eff5c17c5fc21a3302e
baozhoutao <baozhoutao@hotoa.com>
2021-08-09 16:07:24 +0800

    删除旧版 page 代码 #217

--- packages/builder-charts/src/components/index.ts
+++ packages/builder-charts/src/components/index.ts
@@ -1,6 +1,3 @@
 export * from './chart';
 export * from './chartDesign';
 export * from './chartDesignModal';
-export * from './page';
-export * from './dashboard';
-export * from './dashboardGrid';
