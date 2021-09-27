8d3e059c033fe19dd3e272abda288ee60248c3a6
廖大雪 <2291335922@qq.com>
2021-09-27 13:33:04 +0800

    #279 二维码样式优化：元素块样式变大

--- packages/builder-object/src/valueTypes/url.tsx
+++ packages/builder-object/src/valueTypes/url.tsx
@@ -8,7 +8,7 @@ export const url = {
     const { show_as_qr } = field_schema;
     let value= fieldProps.value || text;//ProTable那边fieldProps.value没有值，只能用text
     if(show_as_qr && value && value.length){
-      return (<a href={value} target="_blank"><QRCode value={value} renderAs={"svg"} style={{ width:'60px', height: 'auto', border: '1px solid #888', padding: '2px'}} /></a>)
+      return (<a href={value} target="_blank"><QRCode value={value} renderAs={"svg"} style={{ width:'84px', height: 'auto', border: '1px solid #888', padding: '1px'}} /></a>)
     }
     else{
       return (<a href={value} target="_blank">{value}</a>);
