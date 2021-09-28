506dc2ba0fb61fa4234051bca49e52dffd4dae97
廖大雪 <2291335922@qq.com>
2021-09-28 18:27:07 +0800

    fix bug

--- packages/builder-sdk/src/fetch_etag.ts
+++ packages/builder-sdk/src/fetch_etag.ts
@@ -5,6 +5,7 @@ const etags = {};
 export default (url:any = null, options:any = {headers: {}}) => {
     url = url || options.url; // eslint-disable-line no-param-reassign
 
+    options.method = options.method.toUpperCase();
     if (options.method === 'GET' || !options.method) {
         const etag = etags[url];
         const cachedResponse = data[`${url}${etag}`]; // ensure etag is for url
