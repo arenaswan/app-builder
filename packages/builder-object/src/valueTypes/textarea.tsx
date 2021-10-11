225be4f3025fe37871068558e9892e63c3d71b76
廖大雪 <2291335922@qq.com>
2021-10-11 16:00:09 +0800

    #294 #284 长文本类型字段作为name字段在列表上未显示链接、文字超出时未能显示省略号

--- packages/builder-object/src/valueTypes/textarea.tsx
+++ packages/builder-object/src/valueTypes/textarea.tsx
@@ -1,19 +1,26 @@
 import React from 'react'
 import FieldTextArea from "@ant-design/pro-field/es/components/TextArea";
 
+import "./textarea.less"
+
 export const textarea = {
   render: (text: any, props: any) => {
     const { fieldProps } = props;
     let value = fieldProps.value || props.text;//ProTable那边fieldProps.value没有值，只能用text
-    return (
-      <span title={value}
-        style={{
-          whiteSpace: "pre-wrap"
-        }}
-      >
-        {value}
-      </span>
-    )
+
+    const { field_schema } = fieldProps;
+    const { rows } = field_schema;
+    let newFieldProps =  fieldProps;
+    if(typeof rows === 'number'){
+      newFieldProps = Object.assign({}, fieldProps , {rows})
+    }
+    let renderDom = props.render;
+    if(!props.render){
+      renderDom = (dom: any, record: any)=>{
+        return (<span title={value}>{value}</span>)
+      }
+    }
+    return (<FieldTextArea text={text as string} {...props} render={renderDom} fieldProps={newFieldProps}/>)
   },
   renderFormItem: (text: any, props: any) => {
     const { fieldProps } = props;
