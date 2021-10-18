ec502065c20baabe1eba2903f1a0855e8f91a3b4
baozhoutao <baozhoutao@hotoa.com>
2021-10-18 14:26:14 +0800

    Update App.tsx

--- apps/charts-design/src/App.tsx
+++ apps/charts-design/src/App.tsx
@@ -7,11 +7,20 @@ import { QuerySource } from '@steedos/builder-page';
 // import { QuerySource } from '@steedos/builder-community';
 const initialStore = {
   rootUrl: Settings.rootUrl,
-  tenantId: Settings.tenantId || "immYoecxSfp5vNMRH",
-  userId: Settings.userId || "6161169069c04a1fdcfec2ed",
-  authToken: Settings.authToken || "d3a497b48211d7525d2deb18eff9d1f2660ecdf548b4cdb5f2bcd4d3aa3880a7b05434718ecf1cafca0e9b",
+  tenantId: Settings.tenantId,
+  userId: Settings.userId,
+  authToken: Settings.authToken,
   locale: 'zh_CN'
 }
+
+const isProd = process.env.NODE_ENV === 'production';
+
+if(!isProd){
+  initialStore.tenantId = process.env.DEV_APP_TENANTID;
+  initialStore.userId = process.env.DEV_APP_USERID;
+  initialStore.authToken = process.env.DEV_APP_AUTHTOKEN;
+}
+
 function App() {
   return (
     <div style={{height: '100%'}}>
