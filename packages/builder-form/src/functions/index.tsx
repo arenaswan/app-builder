c9acec06891dc88a7d61900759e9c0bbb3e3b278
廖大雪 <2291335922@qq.com>
2021-11-12 10:24:08 +0800

    #180 当target有值时，元素中才加上该属性

--- packages/builder-form/src/functions/index.tsx
+++ packages/builder-form/src/functions/index.tsx
@@ -4,7 +4,11 @@ import { Apps } from '@steedos-ui/builder-store';
 
 export const getNameFieldColumnRender = (objectApiName: string, linkTarget?: string, nameFieldKey?: string) => {
     return (dom: any, record: any) => {
-        return (<Link title={record[nameFieldKey || 'name']} target={linkTarget} to={getObjectRecordUrl(objectApiName, record._id)} className="text-blue-600 hover:text-blue-500 hover:underline">{dom}</Link>);
+        let props: any = {};
+        if(linkTarget){
+            props.target = linkTarget;
+        }
+        return (<Link title={record[nameFieldKey || 'name']}  to={getObjectRecordUrl(objectApiName, record._id)} {...props} className="text-blue-600 hover:text-blue-500 hover:underline">{dom}</Link>);
     }
 }
 
